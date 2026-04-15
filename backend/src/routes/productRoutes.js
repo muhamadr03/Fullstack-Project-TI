260815395261fe15fdaf1140e95cd7bd29639ee9
mwllnnaa <malmaulana157@gmail.com>
2026-04-15 19:45:33 +0700

    Validasi CRUD Produk bagian Admin (Akmal Maulana)

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -2,13 +2,25 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 const { verifyToken, isAdmin } = require("../middlewares/authMiddleware");
+const Joi = require("joi");
+const { validate } = require("../middlewares/validator");
+
+// Aturan membuat/mengubah produk
+const productSchema = Joi.object({
+  category_id: Joi.number().integer().required(),
+  name: Joi.string().min(3).required(),
+  description: Joi.string().allow(""),
+  price: Joi.number().positive().required(), // Harga tidak boleh minus!
+  stock: Joi.number().integer().min(0).required(), // Stok minimal 0
+  image_url: Joi.string().uri().allow(""),
+});
 
 // Rute Publik
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 
 // Rute Privat (Hanya Admin)
-router.post("/", verifyToken, isAdmin, productController.createProduct);
+router.post("/", verifyToken, isAdmin, validate(productSchema), productController.createProduct);
 router.put("/:id", verifyToken, isAdmin, productController.updateProduct);
 router.delete("/:id", verifyToken, isAdmin, productController.deleteProduct);
 
