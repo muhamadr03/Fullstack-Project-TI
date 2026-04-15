02deb528341317a99fc46b603eb7cd5e9434b695
Ramaaditia123 <adhytt0@gmail.com>
2026-04-15 19:29:03 +0700

    Mengubah, reviewRouters.js

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = express.Router();
 const reviewController = require("../controllers/reviewController");
 const { verifyToken } = require("../middlewares/authMiddleware");
+const Joi = require("joi");
+const { validate } = require("../middlewares/validator");
 
+const reviewSchema = Joi.object({
+  product_id: Joi.number().required(),
+  order_id: Joi.number().required(),
+  rating: Joi.number().integer().min(1).max(5).required(),
+  comment: Joi.string().allow(""),
+});
 // Publik bisa melihat ulasan
 router.get("/:productId", reviewController.getProductReviews);
 
 // Hanya yang login bisa memberi ulasan
-router.post("/", verifyToken, reviewController.addReview);
+router.post("/", verifyToken, validate(reviewSchema), reviewController.addReview);
 
 module.exports = router;
