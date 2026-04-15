43c0ec17de86c99be7285cfc28499d5c09d86eea
Salsabila <sbila884094@gmail.com>
2026-04-15 17:19:13 +0700

    Membuat skema validasi untuk register (Salsabila)

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const Joi = require("joi");
+const { validate } = require("../middlewares/validator");
+const registerSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+  role: Joi.string().valid("customer", "admin").optional()
+});
 
-// Endpoint: POST /api/auth/register
-router.post("/register", authController.register);
-
-// Endpoint: POST /api/auth/login
+router.post("/register", validate(registerSchema), authController.register);
 router.post("/login", authController.login);
 
 module.exports = router;
