a9a0a6c0e66213b255057e4e152184e0c6dff7d3
Nurindah <nuri1306@gmail.com>
2026-04-16 10:30:50 +0700

    Membuat Validator Cart (Nur Indah)

--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
 const { verifyToken } = require("../middlewares/authMiddleware");
+const Joi = require("joi");
+const { validate } = require("../middlewares/validator");
+
+const cartSchema = Joi.object({
+  product_id: Joi.number().required(),
+  quantity: Joi.number().integer().min(1).required(), // Tidak boleh beli 0 atau minus
+});
 
 // Semua rute keranjang WAJIB login (verifyToken), tapi tidak perlu Admin
 router.get("/", verifyToken, cartController.getCart);
-router.post("/", verifyToken, cartController.addToCart);
+router.post("/", verifyToken, validate(cartSchema), cartController.addToCart);
 router.delete("/:id", verifyToken, cartController.removeFromCart);
 
 module.exports = router;
